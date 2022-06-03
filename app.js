5cd558544e007c018461e8582d18b39511a63b4d
Kevin Monteiro <kevin.monteiro96@gmail.com>
2022-06-03 19:13:24 +0100

    feat: add basic ui
    add buttons to play rock paper scissors. At 5 wins reset game

--- app.js
+++ app.js
@@ -18,65 +18,113 @@ function playRound(playerSelection, computerSelection) {
 
     // rock beats scissors
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        result = "player";
+        result = "You win!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        result = "computer";
+        result = "Computer Wins. Boo!";
     }
 
     // scissors beats paper
     if (playerSelection === "scissors" && computerSelection === "paper") {
-        result = "player";
+        result = "You win!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        result = "computer";
+        result = "Computer Wins. Boo!";
     }
 
     // paper beats rock
     if (playerSelection === "paper" && computerSelection === "rock") {
-        result = "player";
+        result = "You win!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        result = "computer";
+        result = "Computer Wins. Boo!";
     }
 
     // draw
     if (playerSelection === computerSelection) {
-        result = "draw";
+        result = "It's a draw";
     }
 
     return result;
 }
 
-function game(playerSelection) {
-    let playerWins = 0;
-    let computerWins = 0;
+function resultAndScores(playerSelection, result) {
+    // play one game and return the result
+    result[0] = playRound(playerSelection, computerPlay());
 
-    for (let i = 0; i < 5; i++) {
-        console.log("Computer: " + computerPlay());
-        console.log("Player: " + playerSelection);
-
-        winner = playRound(playerSelection, computerPlay());
-
-        console.log("--------------------------");
-        console.log("Winner: " + winner);
-        console.log("--------------------------");
-
-        if (winner === "player") {
-            playerWins += 1
-        } else if (winner === "computer") {
-            computerWins += 1
-        }
-    }
-
-    if (playerWins > computerWins) {
-        return `Player wins with ${playerWins} games won`;
-    } else if (computerWins > playerWins) {
-        return `Computer wins with ${computerWins} games won`;
-    } else {
-
+    // increment the winners score
+    if (result[0] === 'You win!') {
+        result[1] += 1;
+        return result;
+    } else if (result[0] === 'Computer Wins. Boo!') {
+        result[2] += 1;
+        return result;
+    } else if (result[0] === "It's a draw") {
+        result[3] += 1;
+        return result;
     }
 }
 
-let playerChoice = prompt("Let's play rock, paper, scissors!");  // needs validation to force string
+function checkWinner(result) {
+    let winner;
 
-console.log("--------------------------");
-console.log(game(playerChoice));
-console.log("--------------------------");
+    // check who has won
+    if (result[1] === 5) {
+        winner = 'player';
+    } else if (result[2] === 5) {
+        winner = 'computer';
+    };
+    
+    // reset scores and return text for winner
+    if (winner === 'player') {
+        return 'Congratulations, you win!!!';
+    } else if (winner === 'computer') {
+        return 'Dun dun dunnnn, computer wins!';
+    }
+};
+
+function resetResults(resultHeading, scores, result) {
+    if (resultHeading.textContent) {
+        scores[0].textContent = 0;
+        scores[1].textContent = 0;
+        scores[2].textContent = 0;
+
+        result[1] = 0;
+        result[2] = 0;
+        result[3] = 0;
+    };
+}
+
+const resultP = document.createElement('p');
+const winnerHeading = document.querySelector('#winner');
+
+const playerScoreP = document.querySelector('#player');
+const computerScoreP = document.querySelector('#computer');
+const drawScoreP = document.querySelector('#draw');
+const scoreArray = [playerScoreP, computerScoreP, drawScoreP]
+
+const resultDiv = document.querySelector('#result');
+const scoresDiv = document.querySelector('#scores');
+
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+let result = ['', 0, 0, 0];
+
+rockButton.addEventListener('click', () => {
+    [resultP.textContent, playerScoreP.textContent, computerScoreP.textContent, drawScoreP.textContent] = resultAndScores('rock', result);
+    winnerHeading.textContent = checkWinner(result);
+    resetResults(winnerHeading, scoreArray, result);
+});
+
+paperButton.addEventListener('click', () => {
+    [resultP.textContent, playerScoreP.textContent, computerScoreP.textContent, drawScoreP.textContent] = resultAndScores('paper', result);
+    winnerHeading.textContent = checkWinner(result);
+    resetResults(winnerHeading, scoreArray, result);
+});
+
+scissorsButton.addEventListener('click', () => {
+    [resultP.textContent, playerScoreP.textContent, computerScoreP.textContent, drawScoreP.textContent] = resultAndScores('scissors', result);
+    winnerHeading.textContent = checkWinner(result);
+    resetResults(winnerHeading, scoreArray, result);
+});
+
+resultDiv.appendChild(resultP);
